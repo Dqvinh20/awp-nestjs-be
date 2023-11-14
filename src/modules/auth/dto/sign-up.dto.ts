ad9280a821ddf113d2f469d92acfded369a342f7
Kwuan <thquan20@clc.fitus.edu.vn>
2023-11-14 23:15:15 +0700

    update signupDTO

--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,10 +1,19 @@
 import {
 	IsEmail,
 	IsNotEmpty,
+	IsOptional,
 	IsStrongPassword,
 	MaxLength,
 } from 'class-validator';
 export class SignUpDto {
+	@IsOptional()
+	@MaxLength(50)
+	first_name: string;
+
+	@IsOptional()
+	@MaxLength(50)
+	last_name: string;
+
 	@IsNotEmpty()
 	@MaxLength(50)
 	@IsEmail()
