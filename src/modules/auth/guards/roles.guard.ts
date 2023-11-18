47e994ff130540d11866e37913f875628cc6a218
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2023-11-18 13:13:06 +0700

    Update roles.guard.ts

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -6,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
-import { IS_PUBLIC_KEY } from 'src/decorators/auth.decorator';
 import { ROLES } from 'src/decorators/roles.decorator';
 import { RequestWithUser } from 'src/types/requests.type';
 
@@ -17,18 +16,15 @@ export class RolesGuard implements CanActivate {
 	canActivate(
 		context: ExecutionContext,
 	): boolean | Promise<boolean> | Observable<boolean> {
-		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-			context.getHandler(),
-			context.getClass(),
-		]);
-		if (isPublic) {
-			return true;
-		}
-
 		const roles: string[] = this.reflector.getAllAndOverride(ROLES, [
 			context.getHandler(),
 			context.getClass(),
 		]);
+
+		if (!roles) {
+			return true;
+		}
+
 		const request: RequestWithUser = context.switchToHttp().getRequest();
 
 		const isAuthorized = roles.includes(request.user.role as unknown as string);
