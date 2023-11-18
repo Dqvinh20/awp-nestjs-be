8afd7af97cfe7dbb2e8adb88f060f734c6dd2e33
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2023-11-18 12:54:17 +0700

    Add ignore public route to roles.guard

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -1,21 +1,40 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+	CanActivate,
+	ExecutionContext,
+	ForbiddenException,
+	Injectable,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from 'src/decorators/auth.decorator';
 import { ROLES } from 'src/decorators/roles.decorator';
 import { RequestWithUser } from 'src/types/requests.type';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-	constructor(private readonly refector: Reflector) {}
+	constructor(private readonly reflector: Reflector) {}
 
 	canActivate(
 		context: ExecutionContext,
 	): boolean | Promise<boolean> | Observable<boolean> {
-		const roles: string[] = this.refector.getAllAndOverride(ROLES, [
+		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+			context.getHandler(),
+			context.getClass(),
+		]);
+		if (isPublic) {
+			return true;
+		}
+
+		const roles: string[] = this.reflector.getAllAndOverride(ROLES, [
 			context.getHandler(),
 			context.getClass(),
 		]);
 		const request: RequestWithUser = context.switchToHttp().getRequest();
-		return roles.includes(request.user.role as unknown as string);
+
+		const isAuthorized = roles.includes(request.user.role as unknown as string);
+		if (!isAuthorized)
+			throw new ForbiddenException("User doesn't have permission");
+
+		return isAuthorized;
 	}
 }
